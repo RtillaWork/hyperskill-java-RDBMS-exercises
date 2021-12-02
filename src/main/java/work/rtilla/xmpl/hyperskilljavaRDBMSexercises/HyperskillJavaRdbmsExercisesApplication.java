435a786fc4cf62a0182973862f44cbf0cf51c08c
naominne <39413995+nauranan@users.noreply.github.com>
2021-12-02 15:45:33 -0700

    Initial commit

---
+++ src/main/java/work/rtilla/xmpl/hyperskilljavaRDBMSexercises/HyperskillJavaRdbmsExercisesApplication.java
@@ -0,0 +1,13 @@
+package work.rtilla.xmpl.hyperskilljavaRDBMSexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperskillJavaRdbmsExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperskillJavaRdbmsExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/rtilla/xmpl/hyperskilljavaRDBMSexercises/HyperskillJavaRdbmsExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package work.rtilla.xmpl.hyperskilljavaRDBMSexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperskillJavaRdbmsExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
