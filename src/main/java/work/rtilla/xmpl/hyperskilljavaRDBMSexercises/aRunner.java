9a53cdc49acd3689a8d93c547cc00345ae97a3e8
naominne <39413995+nauranan@users.noreply.github.com>
2021-12-02 21:38:07 -0700

    add aRunner as CommandLineRunner

---
+++ src/main/java/work/rtilla/xmpl/hyperskilljavaRDBMSexercises/aRunner.java
@@ -0,0 +1,16 @@
+package work.rtilla.xmpl.hyperskilljavaRDBMSexercises;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class aRunner implements CommandLineRunner {
+    private final PasswordGenerator passwordGenerator;
+
+    public aRunner(PasswordGenerator passwordGenerator) {
+        this.passwordGenerator = passwordGenerator;
+    }
+    @Override
+    public void run(String... args) {
+        System.out.printf("A short password: %s\n",passwordGenerator.generate(8));
+        System.out.printf("A long password: %s\n",passwordGenerator.generate(16));
+    }
+}
