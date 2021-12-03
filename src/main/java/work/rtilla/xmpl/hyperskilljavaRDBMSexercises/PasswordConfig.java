5d916da82cb89596865e3f449133e12064bacee6
naominne <39413995+nauranan@users.noreply.github.com>
2021-12-02 21:17:07 -0700

    add PasswordConfig for symbols @Bean

---
+++ src/main/java/work/rtilla/xmpl/hyperskilljavaRDBMSexercises/PasswordConfig.java
@@ -0,0 +1,28 @@
+package work.rtilla.xmpl.hyperskilljavaRDBMSexercises;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PasswordConfig {
+    private static final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMERIC = "0123456789";
+    private static final String SPECIAL_CHARS = "!#@&)(_";
+
+    @Bean
+    public PasswordSymbols allsymbols() {
+        return new PasswordSymbols(ALPHA + NUMERIC + SPECIAL_CHARS);
+    }
+
+    static class PasswordSymbols {
+        private final String symbols;
+
+        public PasswordSymbols(String symbols) {
+            this.symbols = symbols;
+        }
+
+        String getSymbols() {
+            return symbols;
+        }
+    }
+}
