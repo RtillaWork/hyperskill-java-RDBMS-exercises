b158be060afcad27dc9727d59132c0e2640239b9
naominne <39413995+nauranan@users.noreply.github.com>
2021-12-02 21:33:17 -0700

    add PasswordGenerator as @Component

---
+++ src/main/java/work/rtilla/xmpl/hyperskilljavaRDBMSexercises/PasswordGenerator.java
@@ -0,0 +1,27 @@
+package work.rtilla.xmpl.hyperskilljavaRDBMSexercises;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class PasswordGenerator {
+    private static final Random random = new Random();
+    private final PasswordConfig.PasswordSymbols symbols;
+
+    public PasswordGenerator(@Autowired PasswordConfig.PasswordSymbols symbols) {
+        this.symbols = symbols;
+    }
+
+    public String generate(int length) {
+        int symbolsCount = symbols.getSymbols().length();
+
+        StringBuilder password =  new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            password.append(symbols.getSymbols().charAt(random.nextInt(symbolsCount)));
+        }
+
+        return password.toString();
+    }
+}
